ef4e842d345bc20ce898b0c8418697955d003797
EkY <104588608+Ek1Y@users.noreply.github.com>
2023-05-13 23:15:48 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,28 +3,4 @@ game 'gta5'
 lua54 'yes'
 
 author 'EkY'
-
 version '0.0.1'
-
-shared_scripts {
-    '@ox_lib/init.lua',
-    'config.lua'
-}
-
-client_scripts {
-    'client/*',
-    'config.lua'
-}
-
-server_script {
-	'server/*',
-    '@oxmysql/lib/MySQL.lua',
-}	
-
-dependencies {
-	'ox_lib',
-	'es_extended',
-    'oxmysql',
-}
-
-
